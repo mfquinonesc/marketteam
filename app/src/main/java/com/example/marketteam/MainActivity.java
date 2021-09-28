fc4631b001aa2c6111b58028a41cd2ba8e69238e
manuel <manuelfqc@gmail.com>
2021-09-28 15:45:11 -0500

    Inicio del proyecto; primer commit

---
+++ app/src/main/java/com/example/marketteam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.marketteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
