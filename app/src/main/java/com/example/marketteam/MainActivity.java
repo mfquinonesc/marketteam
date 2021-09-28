77a270ee6ca81ff670488fff94acf5e5c626f079
manuel <manuelfqc@gmail.com>
2021-09-28 18:10:15 -0500

    Codigo para llamar la Activity2.

--- app/src/main/java/com/example/marketteam/MainActivity.java
+++ app/src/main/java/com/example/marketteam/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.marketteam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_registro;
+    Button btn_lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_registro= (Button) findViewById(R.id.btn1);
+        btn_lista = (Button) findViewById(R.id.btn2);
+
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(MainActivity.this,MainActivity2.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
+
+
+
 }
